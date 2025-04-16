6488ec26017760c39d15bd40291d039dcbe84e30
lchengzhen <402188181@qq.com>
2025-04-17 00:02:28 +0800

    refactor: 重构构建脚本以使用esbuild替代tsx
    移除 `tsx` 依赖并引入 `esbuild`，重构 `dev.js` 脚本以使用 `esbuild` 进行构建。同时简化 `shared/package.json` 文件，移除不必要的 `main` 字段。

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,4 +1,31 @@
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url';
 import minimist from "minimist";
+import esbuild from "esbuild";
+
+const filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(filename)
+
+console.log(__dirname);
 
 const args = minimist(process.argv.slice(2));
-console.log(args.f);
+const target = args._[0];
+const format = args.f || 'esm';
+
+
+const entry = path.resolve(__dirname, `../packages/${target}/src/index.ts`)
+const outfile = path.resolve(__dirname, `../packages/${target}/dist/index.js`)
+console.log('entry', entry);
+
+esbuild.build({
+    entryPoints: [entry],
+    outfile: outfile,
+    bundle: true,
+    platform: 'browser',
+    format,
+    globalName: 'EasyFile'
+}).then(() => {
+    console.log('Build successful');
+}).catch((error) => {
+    console.error('Build failed', error);
+});
