22e5138f9cb465251f3764336d9bc7a6eec7aecf
lchengzhen <402188181@qq.com>
2025-04-18 17:49:25 +0800

    feat(core): 添加上传完成回调并优化上传任务逻辑
    添加上传完成回调函数 `onComplete`，并在上传任务逻辑中增加错误处理和进度更新。优化了代码结构，确保在上传成功、失败或完成后都能正确调用相应的回调函数。

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -11,22 +11,39 @@ interface UploadOptions {
     chunkSize?: number,
     requestAdapter: UploadRequestAdatpter,
     onProgress?: (progress: number) => void, // 上传进度回调
-    onSuccess?: (result: any) => void, // 上传成功回调
+    onSuccess?: () => void, // 上传成功回调
     onError?: (error: any) => void, // 上传失败回调
+    onComplete?: () => void, // 上传完成回调
 }
 
 // 创建上传任务
 export const createUploadTask = async (options: UploadOptions) => {
-    const { file, chunkSize, requestAdapter } = options;
-    const md5 = await calculateHashWithWorker(file, chunkSize);
-    const { chunks } = splitChunks(file, chunkSize);
-    for (let i = 0; i < chunks.length; i++) {
-        const chunk = chunks[i];
-        // 检查是否已经上传过
-        const isUploaded = await requestAdapter.checkChunk(md5, i);
-        if (isUploaded) {
-            continue;
+    const { file, chunkSize, requestAdapter, onProgress, onSuccess, onError, onComplete } = options;
+    try {
+        const md5 = await calculateHashWithWorker(file, chunkSize);
+        const { chunks, chunkCount } = splitChunks(file, chunkSize);
+        for (let i = 0; i < chunks.length; i++) {
+            const chunk = chunks[i];
+            // 检查是否已经上传过
+            const isUploaded = await requestAdapter.checkChunk(md5, i)
+            if (isUploaded) {
+                continue;
+            }
+            // 上传切片
+            await requestAdapter.uploadChunk(chunk, md5, i);
+            // 更新进度
+            onProgress?.(Math.round((i + 1) / chunkCount * 100));
         }
+        // 合并切片
+        await requestAdapter.mergeChunks();
+        // 上传成功
+        onSuccess?.();
+    } catch (error) {
+        // 上传失败
+        onError?.(error);
+    } finally {
+        // 上传完成
+        onComplete?.();
     }
 }
 
